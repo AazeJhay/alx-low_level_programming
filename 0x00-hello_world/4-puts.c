1b1a25bf3f158d92d3e3530e160f0e3680552042
AazeJhay <josiahaaze@gmail.com>
2023-02-17 14:33:11 -0800

    My first C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #!/bin/bash
-#include <stdio!>
-{
-	puts(Programming is like building a multilingual puzzle\n);
+#include <stdio.h>
+int main(void){
+	puts("Programming is like building a multilingual puzzle\n");
 	return(0);
 }
