f84912ac865981a0ac30986c47e995368b1f2508
AazeJhay <josiahaaze@gmail.com>
2023-02-20 13:51:27 -0800

    I'm loving c programming

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ *
+ * Return: always return 0
+ *
+ */
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 0 ; i <  10 ; i++)
+		putchar(i + 0);
+	for (j = 10 ; j < 16 ; j++)
+		putchar(j);
+	return (0);
+}
