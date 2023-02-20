4b9198849e91e0635a4093928d2903d9cce0c2ba
AazeJhay <josiahaaze@gmail.com>
2023-02-20 03:49:46 -0800

    C program basics continues

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main code
+ *
+ *
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int (n) = 0;
+
+	if (n > 0)
+	{
+		printf("is positive\n");
+	}
+	if (n == 0)
+	{
+		printf("is zero\n");
+	}
+	if (n < 0)
+	{
+		printf("is negative\n");
+	}
+	return (0);
+}
